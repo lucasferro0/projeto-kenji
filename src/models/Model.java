1f4a77ae8e250840d6f197db5ea1f22b99ee6b76
lucasferro0 <lucasferrobrandao@hotmail.com>
2022-11-26 16:28:34 -0300

    Criação a model para facilitar declarações em sql

---
+++ src/models/Model.java
@@ -0,0 +1,73 @@
+package models;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Model 
+{
+    String table;
+
+    String primaryKey;
+
+    public HashMap<String, String> create(HashMap<String, String> data)
+    {
+        ArrayList<String> keys = new ArrayList<String>();
+        ArrayList<String> values = new ArrayList<String>();
+        
+        for (String e : data.keySet()) {
+            keys.add(e);
+        }
+
+        for (String e : data.values()) {
+            values.add(e);
+        }
+
+        ConexaoMySQL conMySql = new ConexaoMySQL();
+
+        Connection con = conMySql.getConexaoMySQL();
+
+        try{
+            String nameColumns = "";
+            for (String e : keys) {
+                nameColumns = nameColumns + e + ", ";
+            }
+
+            String nameValues = "";
+            for (String e : values) {
+                nameValues = nameValues + e + ", ";
+            }
+
+            Statement statement = con.createStatement();
+
+            ResultSet result = statement.executeQuery(
+                "INSERT INTO " + this.table + " (" + nameColumns + ") " + "VALUES " + "(" + nameValues + ")"
+            );
+
+            con.close();
+
+            HashMap<String, String> registroCriado = new HashMap<String, String>();
+            for (String e : keys) {
+                registroCriado.put(e, result.getString(e));
+            }
+
+            return registroCriado;
+
+        }catch (SQLException e){
+            // handle any errors
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
+            return null;
+        }catch(Exception e){
+            // handle any errors
+            System.out.println("SQLException: " + e.getMessage());
+
+            return null;
+        }
+    }
+}
