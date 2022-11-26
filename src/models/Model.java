688a65cdb6f183280f93c15ced389257d81a3467
lucasferro0 <lucasferrobrandao@hotmail.com>
2022-11-26 17:48:20 -0300

    Ajuste na model

--- src/models/Model.java
+++ src/models/Model.java
@@ -6,12 +6,60 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.ResultSetMetaData;
 
 public abstract class Model 
 {
     String table;
 
     String primaryKey;
+
+    public ArrayList<HashMap<String, String>> get(HashMap<String, String> data)
+    {
+        ConexaoMySQL conMySql = new ConexaoMySQL();
+
+        Connection con = conMySql.getConexaoMySQL();
+
+        try{
+            Statement statement = con.createStatement();
+
+            ResultSet result = statement.executeQuery(
+                "SELECT * FROM " + this.table
+            );
+
+            con.close();
+
+            ResultSetMetaData metaRS = result.getMetaData();
+            int columnCount = metaRS.getColumnCount();
+
+            ArrayList<HashMap<String, String>> registros = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> keysValues = new HashMap<String, String>();
+
+            while (result.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    Object value = result.getObject(i);
+                    keysValues.put(metaRS.getColumnName(i), value.toString());
+                }
+
+                registros.add(keysValues);
+            }
+
+            return registros;
+
+        }catch (SQLException e){
+            // handle any errors
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
+            return null;
+        }catch(Exception e){
+            // handle any errors
+            System.out.println("Exception: " + e.getMessage());
+
+            return null;
+        }
+    }
 
     public HashMap<String, String> create(HashMap<String, String> data)
     {
@@ -65,7 +113,7 @@ public abstract class Model
             return null;
         }catch(Exception e){
             // handle any errors
-            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("Exception: " + e.getMessage());
 
             return null;
         }
